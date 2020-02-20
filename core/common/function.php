c5997d6d9e7b176ca3597973e6151429ea494273
candysevenZzz <candy_seven@163.com>
2020-02-20 17:57:19 +0800

    修改打印助手函数P

--- core/common/function.php
+++ core/common/function.php
@@ -1,17 +1,21 @@
 <?php
-//数据打印函数
-function p(...$vars){
-    foreach ($vars as $var){
-        if (is_bool($var)){
-            var_dump($var);
-        }elseif (is_null($var)){
-            var_dump($var);
-        }else{
-            echo "<pre style='position: relative;z-index: 1000;padding: 10px;border-radius: 5px;
+if (!function_exists('p'))
+{
+    //数据打印函数
+    function p(...$vars){
+        foreach ($vars as $var){
+            if (is_bool($var)){
+                var_dump($var);
+            }elseif (is_null($var)){
+                var_dump($var);
+            }else{
+                echo "<pre style='position: relative;z-index: 1000;padding: 10px;border-radius: 5px;
                 background: #F5F5F5;border: 1px solid #aaa;font-size: 14px;line-height: 18px;
                 opacity: 0.9;'>" . print_r($var,true) .
-                "</pre>";
+                    "</pre>";
+            }
         }
-    }
 
+    }  
 }
+
